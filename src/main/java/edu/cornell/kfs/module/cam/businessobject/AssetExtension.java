e715b3cae4fdad12b276183890b38f4f1a5ef36f
db483 <db483@cornell.edu>
2020-02-17 17:05:58 +0000

    Revert "KFSPTS-17866 add KualiCo checkstyle rules and some Cornell specific checks and apply to cam module only"
    This reverts commit e9c2e02cf6103d0c475924e8cc68b593c7b34475.

--- src/main/java/edu/cornell/kfs/module/cam/businessobject/AssetExtension.java
+++ src/main/java/edu/cornell/kfs/module/cam/businessobject/AssetExtension.java
@@ -5,6 +5,7 @@ package edu.cornell.kfs.module.cam.businessobject;
 
 import org.kuali.kfs.krad.bo.PersistableBusinessObjectExtensionBase;
 
+
 /**
  * @author kwk43
  *
@@ -13,35 +14,31 @@ public class AssetExtension extends PersistableBusinessObjectExtensionBase {
 
     private Long capitalAssetNumber;
     private boolean serviceRateIndicator;
-
-    /**
-     * @return the capitalAssetNumber
-     */
-    public Long getCapitalAssetNumber() {
-        return capitalAssetNumber;
-    }
-
-    /**
-     * @param capitalAssetNumber
-     *            the capitalAssetNumber to set
-     */
-    public void setCapitalAssetNumber(Long capitalAssetNumber) {
-        this.capitalAssetNumber = capitalAssetNumber;
-    }
-
-    /**
-     * @return the serviceRateIndicator
-     */
-    public boolean isServiceRateIndicator() {
-        return serviceRateIndicator;
-    }
-
-    /**
-     * @param serviceRateIndicator
-     *            the serviceRateIndicator to set
-     */
-    public void setServiceRateIndicator(boolean serviceRateIndicator) {
-        this.serviceRateIndicator = serviceRateIndicator;
-    }
+	/**
+	 * @return the capitalAssetNumber
+	 */
+	public Long getCapitalAssetNumber() {
+		return capitalAssetNumber;
+	}
+	/**
+	 * @param capitalAssetNumber the capitalAssetNumber to set
+	 */
+	public void setCapitalAssetNumber(Long capitalAssetNumber) {
+		this.capitalAssetNumber = capitalAssetNumber;
+	}
+	/**
+	 * @return the serviceRateIndicator
+	 */
+	public boolean isServiceRateIndicator() {
+		return serviceRateIndicator;
+	}
+	/**
+	 * @param serviceRateIndicator the serviceRateIndicator to set
+	 */
+	public void setServiceRateIndicator(boolean serviceRateIndicator) {
+		this.serviceRateIndicator = serviceRateIndicator;
+	}
     
+    
+	
 }
